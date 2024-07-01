7c9c8e0843d0b29a89c96e29b8f0e0d22c6b5b5d
ASUS <mopsikpvp@gmail.com>
2024-07-01 10:53:44 +0300

    Добавил тест для очков, присваиваемые каждой карте

--- For_tests/test_card.py
+++ For_tests/test_card.py
@@ -7,4 +7,13 @@ def test_Card():
         a = Card(x[i])
         assert a.value == x[i]
 
+def test_hand_point():
+    values = [1, 5, 10, 15, 55, 66, 104]
+    needed_points = [1, 2, 3, 2, 7, 5, 1]
+    actual_points = []
+    for number in values:
+        a = Card(number)
+        actual_points.append(a.point)
+    assert actual_points == needed_points
+
 
